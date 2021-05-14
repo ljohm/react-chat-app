e8c3e8b7af3a18583bfe8a43dedd71758a295fab
yoon1111116 <yunn1209@gmail.com>
2021-05-15 00:53:44 +0900

    wrap app component with authcontext

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
 import AuthService from "./service/auth_service";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const authService = new AuthService();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App authService={authService} />
+    <AuthContextProvider>
+      <App authService={authService} />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
