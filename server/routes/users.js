28d8633dae562a26d16e22bb700350605deece7b
yoon1111116 <yunn1209@gmail.com>
2021-05-14 13:46:39 +0900

    user CRUD

---
+++ server/routes/users.js
@@ -0,0 +1,91 @@
+// const users = [];
+
+// const addUser = ({ id, name, room }) => {
+//   name = name.trim().toLowerCase();
+//   room = room.trim().toLowerCase();
+
+//   const existingUser = users.find(
+//     (user) => user.room === room && user.name === name
+//   );
+
+//   if (existingUser) {
+//     return { error: "Username is taken" };
+//   }
+
+//   const user = { id, name, room };
+//   users.push(user);
+
+//   return { user };
+// };
+
+// const removeUser = (id) => {
+//   const index = users.findIndex((user) => user.id === id);
+//   if (index !== -1) {
+//     return users.splice(index, 1)[0];
+//   }
+// };
+
+// const getUser = (id) => users.find((user) => user.id === id);
+
+// const getUsers = () => users;
+
+// const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+// module.exports = { addUser, removeUser, getUser, getUsers, getUsersInRoom };
+const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+// update
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
+});
+
+// delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account");
+  }
+});
+
+// get user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// router.get("/", (req, res) => {
+//   res.send("server is up and running");
+// });
+
+module.exports = router;
